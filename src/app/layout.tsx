514851180443eb3449f460659d09dd63467b8faf
K-chan41 <keisakata11@gmail.com>
2023-12-04 23:21:47 +0900

    create header design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
 
-const inter = Inter({ subsets: ['latin'] })
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 
-export const metadata: Metadata = {
+export const metadata = {
   title: '技術士1次試験 基礎•適正科目 過去問ドリル',
   description: '公益社団法人日本技術士会が年1回開催する技術士1次試験の基礎科目と適性科目の過去問対策ができるサービス',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
-  )
+  );
 }
