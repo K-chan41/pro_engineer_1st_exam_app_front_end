e5c47637a549b9165b2a13be767326fd5e33b7ce
K-chan41 <keisakata11@gmail.com>
2023-12-08 22:36:09 +0900

    create top-page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,63 @@
-import { Button } from '@mantine/core';
+'use client';
+
+import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem, Tabs } from '@mantine/core';
+import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons-react';
+import { ReportSearch, Ballpen } from 'tabler-icons-react';
+import classes from './HeroBullets.module.css';
 
 export default function Home() {
+  const topImage = 'top-image.svg';
+  const iconStyle = { width: rem(12), height: rem(12) };
+
   return (
     <>
-      TOPページです
+      <Container size="md">
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Text className={classes.text} c="dimmed" mt="md" size="base">
+              公益社団法人日本技術士会が年1回開催する技術士1次試験の基礎科目と適性科目の過去問対策ができるサービスです。過去問を実際に解き、解答を選択し、正解不正解を確認できます。 <br /> 解説を読むことにより、学習をすることができます。 <br /> ユーザー登録をしていただくと、問題にフラグを立てたり、間違えた問題を復習することができます。
+            </Text>
+          </div>
+          <Image src={topImage} className={classes.image} />
+        </div>
+      </Container>
+      <Ballpen
+    size={48}
+    strokeWidth={2}
+    color={'black'}
+  />;
+<ReportSearch
+    size={48}
+    strokeWidth={2}
+    color={'black'}
+  />;
+      <Container>
+        <Tabs radius="md" defaultValue="gallery">
+          <Tabs.List>
+            <Tabs.Tab value="gallery" leftSection={<IconPhoto style={iconStyle} />}>
+              Gallery
+            </Tabs.Tab>
+            <Tabs.Tab value="messages" leftSection={<IconMessageCircle style={iconStyle} />}>
+              Messages
+            </Tabs.Tab>
+            <Tabs.Tab value="settings" leftSection={<IconSettings style={iconStyle} />}>
+              Settings
+            </Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="gallery">
+            Gallery tab content
+          </Tabs.Panel>
+
+          <Tabs.Panel value="messages">
+            Messages tab content
+          </Tabs.Panel>
+
+          <Tabs.Panel value="settings">
+            Settings tab content
+          </Tabs.Panel>
+        </Tabs>
+      </Container>
     </>
   )
 }
--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -5,69 +5,12 @@ import { useDisclosure } from '@mantine/hooks';
 import { IconChevronDown } from '@tabler/icons-react';
 import classes from './HeaderMenu.module.css';
 import Link from 'next/link';
-
-const links = [
-  { link: '/register', label: '新規登録' },
-  { link: '/login', label: 'ログイン' },
-  {
-    link: '#1',
-    label: 'Learn',
-    links: [
-      { link: '/docs', label: 'Documentation' },
-      { link: '/resources', label: 'Resources' },
-      { link: '/community', label: 'Community' },
-      { link: '/blog', label: 'Blog' },
-    ],
-  },
-  { link: '/about', label: 'About' },
-  { link: '/pricing', label: 'Pricing' },
-  {
-    link: '#2',
-    label: 'Support',
-    links: [
-      { link: '/faq', label: 'FAQ' },
-      { link: '/demo', label: 'Book a demo' },
-      { link: '/forums', label: 'Forums' },
-    ],
-  },
-];
+import { HeaderIcon } from './HeaderIcon';
 
 export function HeaderMenu() {
   const [opened, { toggle }] = useDisclosure(false);
   const logoSrc = 'header-logo.png';
   const logoText = '技術士1次試験 基礎•適正科目 過去問ドリル'; 
-
-  const items = links.map((link) => {
-    const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>{item.label}</Menu.Item>
-    ));
-
-    if (menuItems) {
-      return (
-        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
-          <Menu.Target>
-            <a
-              href={link.link}
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
-                <IconChevronDown size="0.9rem" stroke={1.5} />
-              </Center>
-            </a>
-          </Menu.Target>
-          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-        </Menu>
-      );
-    }
-
-    return (
-      <Link key={link.label} href={link.link} className={classes.link}>
-        {link.label}
-      </Link>
-    );
-  });
 
   return (
     <header className={classes.header}>
@@ -76,11 +19,17 @@ export function HeaderMenu() {
           <Link href="/">
             <Center>
                 <Image src={logoSrc} alt="Logo" h={40} />
-                <Text size="xl" fw={900} className={classes.logoText}>{logoText}</Text>
+                <Text size="lg" fw={900} className={classes.logoText}>{logoText}</Text>
             </Center>
           </Link>
           <Group gap={5} visibleFrom="sm">
-            {items}
+            <Link key='/register' href='/register' className={classes.link}>
+              新規登録
+            </Link>
+            <Link key='/login' href='/login' className={classes.link}>
+              ログイン
+            </Link>
+            <HeaderIcon />
           </Group>
           <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
         </div>
