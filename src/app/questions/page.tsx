a1cd0e82bf0fa2380adfc1e3b1266fb862f869fd
K-chan41 <keisakata11@gmail.com>
2023-12-10 18:05:08 +0900

    recieve json

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,33 +1,113 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
+interface QuestionAttributes {
+  number: number;
+  content: string;
+  question_img_src: string | null;
+  correct_answer_no: number;
+  commentary: string;
+  answer_img_src: string | null;
+}
+
+interface Question {
+  id: string;
+  type: string;
+  attributes: QuestionAttributes;
+  relationships: {
+    subject: {
+      data: {
+        id: string;
+        type: string;
+      };
+    };
+    label: {
+      data: {
+        id: string;
+        type: string;
+      };
+    };
+    choices: {
+      data: Array<{
+        id: string;
+        type: string;
+      }>;
+    };
+  };
+}
+
+interface ChoiceAttributes {
+  content: string;
+  order: number;
+}
+
+interface Choice {
+  id: string;
+  type: string;
+  attributes: ChoiceAttributes;
+  relationships: {
+    question: {
+      data: {
+        id: string;
+        type: string;
+      };
+    };
+  };
+}
+
+interface LabelAttributes {
+  number: number;
+  title: string;
+}
+
+interface Label {
+  id: string;
+  type: string;
+  attributes: LabelAttributes;
+  relationships: {
+    questions: {
+      data: Array<{
+        id: string;
+        type: string;
+      }>;
+    };
+  };
+}
+
+interface ApiResponse {
+  data: Array<Question | Choice | Label>;
+}
+
 export default function QuestionsPage() {
-  const router = useRouter();
-  const [questions, setQuestions] = useState([]);
+  const searchParams = useSearchParams();
+  const [questions, setQuestions] = useState<Question[]>([]);
 
   useEffect(() => {
-    if (router.isReady) {
-      const { subject_ids } = router.query;
+    const subjectIds = searchParams.get('subject_ids[]');
 
-      if (subject_ids) {
-        const query = Array.isArray(subject_ids)
-          ? subject_ids.map(id => `subject_ids[]=${id}`).join('&')
-          : `subject_ids[]=${subject_ids}`;
+    if (subjectIds) {
+      const query = Array.isArray(subjectIds)
+        ? subjectIds.map(id => `subject_ids[]=${id}`).join('&')
+        : `subject_ids[]=${subjectIds}`;
 
-        fetch(`http://localhost:4000/api/v1/questions/filter?${query}`)
-          .then(response => response.json())
-          .then(data => setQuestions(data.data));
-      }
+      fetch(`http://localhost:4000/api/v1/questions/filter?${query}`)
+        .then(response => response.json())
+        .then((responseData: ApiResponse) => {
+          // responseData.data で直接データを取得
+          setQuestions(responseData.data.filter(item => item.type === 'question'));
+        });
     }
-  }, [router.isReady, router.query]);
+  }, [searchParams]);
+
+  console.log(questions);
 
   return (
     <div>
       {questions.map((question, index) => (
         <div key={index}>
-          <p>{question.content}</p>
+          <p>{question.attributes.content}</p>
           {/* その他の質問に関連するデータを表示 */}
         </div>
       ))}
