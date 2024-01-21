5f2b3b6c9da647d5d51ab1a077cc56d50b55198a
K-chan41 <keisakata11@gmail.com>
2024-01-21 21:38:02 +0900

    fix: registor_login_form

--- src/components/AuthenticationForm.tsx
+++ src/components/AuthenticationForm.tsx
@@ -38,7 +38,7 @@ export function AuthenticationForm(props: PaperProps) {
       name: (val) => type === 'register' && (val.trim().length < 2 ? '名前は2文字以上で入力してください' : null),
       email: (val) => (/^\S+@\S+$/.test(val) ? null : '有効なメールアドレスを入力してください'),
       password: (val) => (val.length < 6 ? 'パスワードには少なくとも6文字を含める必要があります' : null),
-      terms: (value) => (value === true ? null : '利用規約とプライバシーポリシーに同意する必要があります'),
+      terms: (value) => type === 'register' && (value !== true ? '利用規約とプライバシーポリシーに同意する必要があります' : null),
     },
   });
 
